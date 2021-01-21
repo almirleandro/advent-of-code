02d648b5068243058ab2ea4a298acc16421352a6
Almir Leandro <leandroalmm50@gmail.com>
2021-01-21 18:02:19 -0300

    Delete comments in index.js

--- year 2015/day1/index.js
+++ year 2015/day1/index.js
@@ -14,33 +14,7 @@ fs.readFile('./directions.txt', (err, data) => {
     if (!basement) floor === -1 ? basement = charposition : null;
     charposition === 7000 ? null : charposition++;
   }
-  console.log("I've seen you followed the right instructions!")
-  console.log(`You have reached floor #${floor}`)
-  console.log(`You have reached the basement at the #${basement} position`)
-
-
-  // const goUp = '(';
-  // const goDown = ')';
-
-  // let pos1 = 0;
-  // let upCount = 0;
-  // while (true) {
-  //   let foundPos = str.indexOf(goUp, pos1);
-  //   if (foundPos == -1) break;
-  //   upCount++;
-  //   pos1 = foundPos + 1; // continue the search from the next position
-  // }
-  // console.log(`Go up ${upCount} floors!`)
-
-  // let pos2 = 0;
-  // let downCount = 0;
-  // while (true) {
-  //   let foundPos = str.indexOf(goDown, pos2);
-  //   if (foundPos == -1) break;
-  //   downCount++;
-  //   pos2 = foundPos + 1; // continue the search from the next position
-  // }
-  // console.log(`Go down ${downCount} floors!`)
-
-  // console.log(`You have reached floor #${upCount - downCount}`)
+  console.log("I've seen you followed the right instructions!");
+  console.log(`You have reached floor #${floor}`);
+  console.log(`You have reached the basement at the #${basement} position`);
 })
