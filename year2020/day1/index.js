42dc2d21b0d04651157d9e83163bf175b40cfbae
Almir Leandro <leandroalmm50@gmail.com>
2021-02-11 18:09:32 -0300

    Add second challenge from 2020, Day 1

--- year2020/day1/index.js
+++ year2020/day1/index.js
@@ -3,16 +3,34 @@ const fs = require('fs');
 fs.readFile('./input.txt', (err, data) => {
   if (err) throw err;
 
+  // Get the input and change it to an array
   const str = data.toString();
-
   const arr = str.split("\r\n");
 
+  let foundTri = false;
+  let foundDuo = false;
+
+  // Loop through the array
   outer: for (let num1 of arr) {
+
     for (let num2 of arr) {
-      if (+num1 + +num2 === 2020) {
-        console.log(`numbers ${num1} and ${num2}, which multiplied are ${num1 * num2}`);
-        break outer;
+
+      // Do this loop if the answers is not known yet
+      if (!foundTri) for (let num3 of arr) {
+        if (+num1 + +num2 + +num3 === 2020) {
+          console.log(`Second answer: numbers are ${num1} and ${num2} and ${num3}, which multiplied are ${num1 * num2 * num3}`);
+          foundTri = true;
+        }
       }
+
+      // Print the answer when we have it
+      if (+num1 + +num2 === 2020) {
+        console.log(`First answer: numbers are ${num1} and ${num2}, which multiplied are ${num1 * num2}`);
+        foundDuo = true;
+      }
+
+      // Break the loop if we have the answers already
+      if (foundDuo && foundTri) break outer;
     }
   }
 })
