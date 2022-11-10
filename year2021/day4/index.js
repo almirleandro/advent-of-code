04edb29e5f329b76a67f8ce2d1d6d99d628486c2
Almir Marques <almir.marques@valtech.com>
2022-11-10 09:56:31 -0300

    Add solution to 2021 day 4 part 1

--- year2021/day4/index.js
+++ year2021/day4/index.js
@@ -16,10 +16,11 @@ fs.readFile('./input.txt', (err, data) => {
       currentBlock.push(line)
     } else {
       inputBlocks.push(currentBlock);
-      currentBlock = []; // clears the array
+      currentBlock = []; // Clears the array
     }
   }
   inputBlocks.push(currentBlock);
+  inputBlocks.shift();
   // console.log(inputBlocks)
 
   const bingoCards = [];
@@ -27,94 +28,68 @@ fs.readFile('./input.txt', (err, data) => {
   for (const card of inputBlocks) {
     for (const rowString of card) {
       let rowArray = rowString.split(" ");
-      rowArray = rowArray.filter(n => n);
+      rowArray = rowArray.filter(n => n); // Remove empty values
       currentCard.push(rowArray)
     }
-    bingoCards.push(currentCard);
+    const cardScore = {
+      col1: 0,
+      col2: 0,
+      col3: 0,
+      col4: 0,
+      col5: 0,
+      row1: 0,
+      row2: 0,
+      row3: 0,
+      row4: 0,
+      row5: 0,
+    }
+    bingoCards.push({card: currentCard, cardScore});
     currentCard = [];
   }
-  console.log(bingoCards[100])
+  // console.log(bingoCards)
   
   const bingoOrder = arr[0].split(",")
   // console.log(bingoOrder)
 
+  let wasWinnerAnounced = false;
+  for (const currentNumber of bingoOrder) { // --------------------------------------- Bingo round
+    for (const cardObj of bingoCards) { // ------------------------------------------- Check card
+      for (let rowIndex = 0; rowIndex < cardObj.card.length; rowIndex++) { // -------- Check row
+        const row = cardObj.card[rowIndex];
+        for (let colIndex = 0; colIndex < row.length; colIndex++) { // --------------- Check column
+          const number = row[colIndex];
+          if (number === currentNumber) { // ----------------------------------------- Number check
+            row[colIndex] = "x"; // -------------------------------------------------- Mark the spot
+            cardObj.cardScore[`col${colIndex + 1}`]++;
+            cardObj.cardScore[`row${rowIndex + 1}`]++;
+            if (cardObj.cardScore[`col${colIndex + 1}`] === 5 ||
+                cardObj.cardScore[`row${rowIndex + 1}`] === 5) {
+              anounceWinner({card: cardObj.card, cardScore: cardObj.cardScore, winningNumber: number}); // -- Bingo!
+            }
+          }
+        }
+      }
+    }
+  }
+
+  function anounceWinner(winner) {
+    if (!wasWinnerAnounced) {
+      console.log(winner);
+      wasWinnerAnounced = true;
+      calculateFinalScore(winner);
+    }
+  }
+
+  function calculateFinalScore(winner) {
+    let unmarkedNumbersSum = 0;
+    for (const row of winner.card) {
+      for (const number of row) {
+        if (number !== "x") unmarkedNumbersSum += parseInt(number);
+      }
+    }
+    console.log("Final score is " + (unmarkedNumbersSum * winner.winningNumber))
+  }
 })
 
 
-// -------------------- Part 2 --------------------
-// const fs = require('fs');
-
-// fs.readFile('./input.txt', (err, data) => {
-//   if (err) throw err;
-
-//   // Get the input and change it to an array
-//   const str = data.toString();
-//   const arr = str.split("\r\n");
-
-//   let relevantBinaries = arr;
-//   let focusBit = 0;
-
-//   do {
-//     let temporaryBinaries1 = [];
-//     let temporaryBinaries0 = [];
-
-//     // Positive is 1
-//     // Negative is 0
-//     let winningBit = 0;
-
-//     for (const binary of relevantBinaries) {
-//       if (binary[focusBit] === "1") {
-//         winningBit++;
-//         temporaryBinaries1.push(binary)
-//       } else {
-//         winningBit--;
-//         temporaryBinaries0.push(binary)
-//       }
-//     }
-
-//     if (winningBit < 0) {
-//       relevantBinaries = temporaryBinaries0;
-//     } else {
-//       relevantBinaries = temporaryBinaries1;
-//     }
-//     focusBit++
-    
-//   } while (relevantBinaries.length > 1);
-
-//   const oxygen = parseInt(relevantBinaries[0], 2);
-
-
-//   relevantBinaries = arr;
-//   focusBit = 0;
-
-//   do {
-//     let temporaryBinaries1 = [];
-//     let temporaryBinaries0 = [];
-
-//     // Positive is 1
-//     // Negative is 0
-//     let winningBit = 0;
-
-//     for (const binary of relevantBinaries) {
-//       if (binary[focusBit] === "1") {
-//         winningBit++;
-//         temporaryBinaries1.push(binary)
-//       } else {
-//         winningBit--;
-//         temporaryBinaries0.push(binary)
-//       }
-//     }
-
-//     if (winningBit < 0) {
-//       relevantBinaries = temporaryBinaries1;
-//     } else {
-//       relevantBinaries = temporaryBinaries0;
-//     }
-//     focusBit++
-    
-//   } while (relevantBinaries.length > 1);
-
-//   const carbon = parseInt(relevantBinaries[0], 2);
-
-//   console.log(carbon * oxygen)
-// })
+// -------------------- Part 2 (TBD) --------------------
