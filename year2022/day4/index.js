2f541cff497447c591a1b094b3ebb56f2c54ac4d
Almir Marques <almir.marques@valtech.com>
2022-12-08 19:33:24 -0300

    Add part 1 solution to 2022 day 4

--- year2022/day4/index.js
+++ year2022/day4/index.js
@@ -8,34 +8,34 @@ fs.readFile('./input.txt', (err, data) => {
   const str = data.toString();
   const input = str.split("\r\n");
 
-  // for (const line of input) {
-  for (let index = 0; index < 20; index++) { // Testing with first lines
-  const line = input[index];
+  let overlapingCount = 0;
+
+  for (const line of input) {
+  // for (let index = 0; index < 20; index++) { // Testing with first lines
+  // const line = input[index];
 
     const [firstSection, secondSection] = line.split(",");
     const [firstSectionStart, firstSectionEnd] = firstSection.split("-");
     const [secondSectionStart, secondSectionEnd] = secondSection.split("-");
 
     switch (true) {
+      case (
+        parseInt(firstSectionStart) === parseInt(secondSectionStart) ||
+        parseInt(firstSectionEnd) === parseInt(secondSectionEnd)
+      ):
+        overlapingCount += 1;
+        break;
+
       case (parseInt(firstSectionStart) > parseInt(secondSectionStart)):
-        
+        if (parseInt(firstSectionEnd) < parseInt(secondSectionEnd)) overlapingCount += 1;
         break;
 
       case (parseInt(firstSectionStart) < parseInt(secondSectionStart)):
-        
-        break;
-
-        case (parseInt(firstSectionStart) === parseInt(secondSectionStart)):
-        
+        if (parseInt(firstSectionEnd) > parseInt(secondSectionEnd)) overlapingCount += 1;
         break;
     }
-
-    console.log({
-      firstSectionStart,
-      secondSectionStart,
-      "doesFirstSectionStartLater": parseInt(firstSectionStart) > parseInt(secondSectionStart)
-    })
   }
+  console.log(overlapingCount)
 })
 
 
@@ -43,14 +43,17 @@ fs.readFile('./input.txt', (err, data) => {
  * 
  * - Read first line of input
  * - Separate the pair and get each number
+ * 
+ * + If the 1st number of the 1st range is equal to the 1st number of the 2nd range
+ *   add 1 to the count.
+ * + If the 2nd number of the 1st range is equal to the 2nd number of the 2nd range
+ *   add 1 to the count.
+ * 
  * - Compare the 1st number of the 1st range with the 1st number of the 2nd range
  *   to see which is bigger
  * - Compare the 2nd number of this range with the 2nd number of the other range
  *   to see if it is smaller
  * - If it is, add 1 to the count.
- * 
- * + If the 1st number of the 1st range is equal to the 1st number of the 2nd range
- *   add 1 to the count.
  * 
 */
 
